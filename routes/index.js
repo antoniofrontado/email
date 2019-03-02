8a8faef7e5e05598428e21fa9c1d6c60d5c5ebe6
antoniofrontado <frontado.antonio@gmail.com>
2019-03-02 12:56:33 -0400

    rutas de acceso

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-router.post('/', function(req, res, next) {
-  res.render('apituloenvias');
+router.post('/apituloEnvias', function(req, res, next) {
+  res.render('index');
 });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,6 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 router.post('/', function(req, res, next) {
-  res.render('apituloenvias');
+  res.render('apituloEnvias');
 });
 module.exports = router;
